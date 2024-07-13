25b1fef94f0a44ecf326060b8abfd1c3c617b2a1
dachy <131180149+dachy21@users.noreply.github.com>
2024-07-13 17:23:54 +0400

    ეს არის For I In range სადაც შენ ჩაწერ რაიმე სიტყვას და დაგიბეჭდავს 50 სიტყვას

---
+++ range.py
@@ -0,0 +1,12 @@
+#Making Text For i in range 
+import time
+
+def function():
+        name = str(input('Chaweret Ragindat Rom Gaspamot:'))
+        time.sleep(1)
+
+        for _ in range(50):
+                print(name , {_})
+           
+
+function()
